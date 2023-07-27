ea6ea292f9cb20b7a78a83093b66e9a2da2ca206
Benoît Guérout <bguerout@gmail.com>
2023-07-27 15:25:49 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -2,65 +2,80 @@
 #include <HTTPClient.h>
 #include "config.hpp"
 
-const char* ssid = THERMOSTAT_WIFI_SSID;
-const char* wifiPassword = THERMOSTAT_WIFI_PASSWORD;
+const char *ssid = THERMOSTAT_WIFI_SSID;
+const char *wifiPassword = THERMOSTAT_WIFI_PASSWORD;
 const int relayPin = THERMOSTAT_RELAY_PIN;
-const char* url = "https://raw.githubusercontent.com/bguerout/esp-remote-relay/main/data/garden";
+const int httpTimeout = 5000;
 
-void startWiFi() {
-  Serial.print("\r\nConnecting to: "); Serial.println(String(ssid));
+void startWiFi()
+{
+  Serial.print("\r\nConnecting to: ");
+  Serial.println(String(ssid));
   IPAddress dns(8, 8, 8, 8); // Use Google as DNS
   WiFi.disconnect();
-  WiFi.mode(WIFI_STA);       // switch off AP
+  WiFi.mode(WIFI_STA); // switch off AP
   WiFi.setAutoConnect(true);
   WiFi.setAutoReconnect(true);
   WiFi.begin(ssid, wifiPassword);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(50);
   }
   Serial.println("\nWiFi connected at: " + WiFi.localIP().toString());
 }
 
-bool checkJsonForCloseState() {
+bool shouldActivate(const String &url)
+{
   HTTPClient http;
-  http.begin(url);
+  WiFiClient wifiClient;
+  wifiClient.setTimeout(httpTimeout);
 
+  http.begin(wifiClient, url);
   int httpResponseCode = http.GET();
-  if (httpResponseCode == HTTP_CODE_OK) {
+
+  if (httpResponseCode == HTTP_CODE_OK)
+  {
     String payload = http.getString();
-    Serial.println("Response payload: " + payload);
     http.end();
 
-    // Check if the JSON response contains "state":"close"
-    if (payload.indexOf("close") != -1) {
+    Serial.println("Response payload: " + payload);
+    
+    if (payload.indexOf("close") != -1)
+    {
       return true;
-    } else {
+    }
+    else
+    {
       return false;
     }
-  } else {
-    Serial.print("HTTP request failed with error code: ");
-    Serial.println(httpResponseCode);
+  }
+  else
+  {
+    Serial.println("HTTP request failed with error code: " + httpResponseCode);
     http.end();
     return false;
   }
 }
 
-void setup() {
+void setup()
+{
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, HIGH); // Initially, keep the relay open
   Serial.begin(9600);
   startWiFi();
 }
 
-void loop() {
-  // Check if the relay should be closed based on the JSON response
-  if (checkJsonForCloseState()) {
-    digitalWrite(relayPin, LOW); // Close the relay
-  } else {
-    digitalWrite(relayPin, HIGH); // Keep the relay open
+void loop()
+{
+  if (shouldActivate("https://pastebin.com/raw/jmV9Jyaz"))
+  {
+    digitalWrite(relayPin, LOW);
+  }
+  else
+  {
+    digitalWrite(relayPin, HIGH);
   }
 
-  // Wait for some time before making the next request
-  delay(10000); // 5 seconds
+  delay(300000); // 5 minutes
 }
